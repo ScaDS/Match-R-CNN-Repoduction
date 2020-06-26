9b5a3e45b4334da53800c9bffeddf1e45d01085b
ssd-ls-a <stefan.schmidt-dichte@stud.htwk-leipzig.de>
2020-06-26 11:54:46 +0200

    [ADD] project structure

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Match-R-CNN-Repoduction
 Reproducing the fashion image categorization and retrieval baseline approach from https://github.com/switchablenorms/DeepFashion2 / https://arxiv.org/pdf/1901.07973.pdf
+
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Reimplementation of Match R-CNN',
+    author='Stefn Schmidt-Dichte',
+    license='MIT',
+)
