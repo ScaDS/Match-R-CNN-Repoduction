f13f8cc3441d05d5491f13b6b17789af041e1958
Stefan Schmidt-Dichte <stefan.schmidt-dichte@stud.htwk-leipzig.de>
2020-09-01 13:30:00 +0200

    [CHANGES] register_dataset_to_detectron2.py

--- src/data/register_dataset_to_detectron2.py
+++ src/data/register_dataset_to_detectron2.py
@@ -1,12 +1,13 @@
-import cv2
 import os
 
-from detectron2.data import MetadataCatalog, DatasetCatalog, build_detection_test_loader
-from detectron2.engine import DefaultTrainer
+import torch
+from detectron2.data import MetadataCatalog, DatasetCatalog, build_detection_test_loader, build_detection_train_loader
+from detectron2.engine import DefaultTrainer, HookBase
 from detectron2.config import get_cfg
 from detectron2.evaluation import COCOEvaluator, inference_on_dataset
 from detectron2.model_zoo import model_zoo
 from detectron2.data.datasets import register_coco_instances
+from detectron2.utils import comm
 
 import settings
 
@@ -41,15 +42,17 @@ register_coco_instances("test",
 train_metadata = MetadataCatalog.get('train')
 dataset_dicts = DatasetCatalog.get('train')
 
+
 cfg = get_cfg()
 cfg.merge_from_file(model_zoo.get_config_file('COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml'))
 cfg.DATASETS.TRAIN = ('train',)
 cfg.DATASETS.TEST = ('test',)
+cfg.DATASETS.VAL = ('validation',)
 cfg.DATALOADER.NUM_WORKERS = 2
 cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url('COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml')
 cfg.SOLVER.IMS_PER_BATCH = 2
 cfg.SOLVER.BASE_LR = 0.02
-cfg.SOLVER.MAX_ITER = 300
+cfg.SOLVER.MAX_ITER = 3000
 cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 128
 cfg.MODEL.ROI_HEADS.NUM_CLASSES = 13
 
@@ -60,5 +63,3 @@ trainer.train()
 evaluator = COCOEvaluator('validation', cfg, False, output_dir="./output/")
 val_loader = build_detection_test_loader(cfg, 'validation')
 print(inference_on_dataset(trainer.model, val_loader, evaluator))
-
-
