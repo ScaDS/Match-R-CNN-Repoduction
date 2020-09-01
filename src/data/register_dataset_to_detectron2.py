d9f3ac6efeff70ac0f7fef09857961a37ddb6659
Stefan Schmidt-Dichte <stefan.schmidt-dichte@stud.htwk-leipzig.de>
2020-09-01 14:19:22 +0200

    [CHANGES] register_dataset_to_detectron2.py

--- src/data/register_dataset_to_detectron2.py
+++ src/data/register_dataset_to_detectron2.py
@@ -1,13 +1,39 @@
 import os
 
-from detectron2.data import MetadataCatalog, DatasetCatalog, build_detection_test_loader
-from detectron2.engine import DefaultTrainer
+import torch
+from detectron2.data import MetadataCatalog, DatasetCatalog, build_detection_test_loader, build_detection_train_loader
+from detectron2.engine import DefaultTrainer, HookBase
 from detectron2.config import get_cfg
 from detectron2.evaluation import COCOEvaluator, inference_on_dataset
 from detectron2.model_zoo import model_zoo
 from detectron2.data.datasets import register_coco_instances
+from detectron2.utils import comm
 
 import settings
+
+
+class ValidationLoss(HookBase):
+    def __init__(self, cfg):
+        super().__init__()
+        self.cfg = cfg.clone()
+        self.cfg.DATASETS.TRAIN = cfg.DATASETS.VAL
+        self._loader = iter(build_detection_train_loader(self.cfg))
+
+    def after_step(self):
+        data = next(self._loader)
+        with torch.no_grad():
+            loss_dict = self.trainer.model(data)
+
+            losses = sum(loss_dict.values())
+            assert torch.isfinite(losses).all(), loss_dict
+
+            loss_dict_reduced = {"val_" + k: v.item() for k, v in
+                                 comm.reduce_dict(loss_dict).items()}
+            losses_reduced = sum(loss for loss in loss_dict_reduced.values())
+            if comm.is_main_process():
+                self.trainer.storage.put_scalars(total_val_loss=losses_reduced,
+                                                 **loss_dict_reduced)
+
 
 register_coco_instances('train',
                         {},
@@ -45,6 +71,7 @@ cfg = get_cfg()
 cfg.merge_from_file(model_zoo.get_config_file('COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml'))
 cfg.DATASETS.TRAIN = ('train',)
 cfg.DATASETS.TEST = ('validation',)
+cfg.DATASETS.VAL = ('validation',)
 cfg.DATALOADER.NUM_WORKERS = 2
 cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url('COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml')
 cfg.SOLVER.IMS_PER_BATCH = 2
@@ -52,10 +79,17 @@ cfg.SOLVER.BASE_LR = 0.02
 cfg.SOLVER.MAX_ITER = 3000
 cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 128
 cfg.MODEL.ROI_HEADS.NUM_CLASSES = 13
-cfg.TEST.EVAL_PERIOD = 100
+# cfg.TEST.EVAL_PERIOD = 100
 
 os.makedirs(cfg.OUTPUT_DIR, exist_ok=True)
 trainer = DefaultTrainer(cfg)
+
+val_loss = ValidationLoss(cfg)
+trainer.register_hooks([val_loss])
+# swap the order of PeriodicWriter and ValidationLoss
+trainer._hooks = trainer._hooks[:-2] + trainer._hooks[-2:][::-1]
+
+
 trainer.resume_or_load(resume=False)
 trainer.train()
 evaluator = COCOEvaluator('validation', cfg, False, output_dir="./output/")
